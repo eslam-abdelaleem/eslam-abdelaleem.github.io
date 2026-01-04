43cd59547f7b9c4164bd106e646e2d60e9406784
eslam-abdelaleem <144143303+eslam-abdelaleem@users.noreply.github.com>
2026-01-04 23:12:20 +0000

    Deploying to gh-pages from @ eslam-abdelaleem/eslam-abdelaleem.github.io@c004c148a92447fb46bd4474b0d40ad74782fddb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,29 +58,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-ai-reveals-unexpected-new-physics-in-dusty-plasma-emory-university-news-july-2025-feature-on-pnas-2025-paper-covered-by-21-news-outlets-podcasts-and-blogs",
+        },{id: "news-ai-reveals-unexpected-new-physics-in-dusty-plasma-emory-university-news-july-2025-feature-on-pnas-2025-paper-covered-by-21-news-outlets-podcasts-and-blogs",
           title: '“AI Reveals Unexpected New Physics in Dusty Plasma.” Emory University News, July 2025....',
           description: "",
           section: "News",},{id: "news-periodic-table-for-ai-methods-aims-to-drive-innovation-emory-university-news-december-2025-feature-on-jmlr-2025-paper-deep-variational-multivariate-information-bottleneck",
@@ -90,7 +68,7 @@ ninja.data = [{
           title: 'CoCo Pilot Grant',
           description: "Tracking the Trajectory of Adaptation",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/cocopilot/";
+              window.location.href = "/projects/cocopilot.html";
             },},{
         id: 'social-cv',
         title: 'CV',
